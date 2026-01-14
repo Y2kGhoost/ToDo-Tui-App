332645c8d45d2cdbe5e6102d0c39bf86fdf45f4b
Y2kGhoost <ilyasslite112@gmail.com>
2026-01-14 16:30:17 +0100

    Fixing the dir/Folder Creation

--- todo.go
+++ todo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 type Task struct {
@@ -11,30 +12,41 @@ type Task struct {
 	Done  bool   `json:"done"`
 }
 
-const TodoFile = "./data/tasks.json"
+const (
+	dataDir  = "data"
+	fileName = "tasks.json"
+)
+
+func SaveTasks(tasks []Task) error {
+	err := os.MkdirAll(dataDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	fullPath := filepath.Join(dataDir, fileName)
+
+	data, err := json.MarshalIndent(tasks, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// 4. Write the file
+	return os.WriteFile(fullPath, data, 0644)
+}
 
 func LoadTasks() ([]Task, error) {
-	if _, err := os.Stat(TodoFile); os.IsNotExist(err) {
+	fullPath := filepath.Join(dataDir, fileName)
+
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
 		return []Task{}, nil
 	}
 
-	data, err := os.ReadFile(TodoFile)
+	data, err := os.ReadFile(fullPath)
 	if err != nil {
 		return nil, err
 	}
 
 	var tasks []Task
-	if err := json.Unmarshal(data, &tasks); err != nil {
-		return nil, err
-	}
-
-	return tasks, nil
-}
-
-func SaveTasks(tasks []Task) error {
-	data, err := json.MarshalIndent(tasks, "", "	")
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(TodoFile, data, 0644)
+	err = json.Unmarshal(data, &tasks)
+	return tasks, err
 }
